76bd70c2e2bb05a95514dd3e7c963d6e10477773
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-05-25 14:38:06 -0400

    added card autofilling

--- deckbuilder/src/App.js
+++ deckbuilder/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, TextField, MenuItem, Paper } from '@material-ui/core';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        value: "",
+        suggestions: [],
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div id="search-input">
+                    <TextField id="Card Name" value={this.state.value} onChange={(e) => {
+                        this.setState({ value: e.target.value, anchor: e.target});
+                        fetch("https://api.scryfall.com/cards/autocomplete?q=" + e.target.value).then(response => {
+                            if (!response.ok) {
+                                throw Error('Network request failed.')
+                            }
+                            return response;
+                        }).then(data => data.json()).then(data => {
+                            this.setState({ suggestions: data.data });
+                        });
+                    }} style={{ width: "100%" }} />
+                    {
+                        this.state.suggestions.length > 0 ? (
+                            <Paper style={{ maxHeight: 300, overflowY: "scroll" }} onClose={this.handleClose}>
+                                {this.state.suggestions.map((value, i) => (
+                                    <MenuItem key={i} onClick={() => {
+                                        this.setState({ value: value, suggestions: [] });
+                                    }}>{value}</MenuItem>
+                                ))}
+                            </Paper>
+                        ) : null
+                    }
+                </div>
+                <Button id="search" variant="contained" color="primary">
+                    Search
+                </Button>
+             </div>
+        );
+    }
+
 }
 
 export default App;
